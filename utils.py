dfc7e04ff0aa55ed60cedba55d7dd06616bf4e44
Chandrashekar, Girish <Girish.Chandrashekar@netapp.com>
2024-02-24 05:41:46 +0530

    re-arrange files

---
+++ utils.py
@@ -0,0 +1,88 @@
+from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+from tqdm import tqdm
+
+def get_data_transforms():
+    train_transforms = transforms.Compose([
+        transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+        transforms.Resize((28, 28)),
+        transforms.RandomRotation((-15., 15.), fill=0),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    return train_transforms, test_transforms
+
+def get_data_loaders(batch_size, train_transforms, test_transforms):
+    train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+    test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
+
+    kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
+
+    train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
+
+    return train_loader, test_loader
+
+def get_optimizer(model):
+    optimizer = optim.SGD(model.parameters(), lr=10.01, momentum=0.9)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1, verbose=True)
+    return optimizer, scheduler
+
+def get_criterion():
+    return nn.CrossEntropyLoss(reduction='sum')
+
+def train(model, device, train_loader, optimizer, criterion):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        pred = model(data)
+
+        # Calculate loss
+        loss = criterion(pred, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        pred = pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    return train_loss, correct / processed
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    return test_loss, correct / len(test_loader.dataset)
